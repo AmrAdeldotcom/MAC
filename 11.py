73dc3fb5f0cfb1f8e3d556a7524d830ce3732d70
Amr Adel Abdelnaby Abdalla <amradel.com@gmail.com>
2018-02-05 16:41:05 +0200

    Madlib lesson 11

---
+++ 11.py
@@ -0,0 +1,54 @@
+# Let's put it all together. Write code for the function process_madlib, which takes in
+# a string "madlib" and returns the string "processed", where each instance of
+# "NOUN" is replaced with a random noun and each instance of "VERB" is
+# replaced with a random verb. You're free to change what the random functions
+# return as verbs or nouns for your own fun, but for submissions keep the code the way it is!
+
+from random import randint
+
+
+def random_verb():
+    random_num = randint(0, 1)
+    if random_num == 0:
+        return "run"
+    else:
+        return "kayak"
+
+
+def random_noun():
+    random_num = randint(0, 1)
+    if random_num == 0:
+        return "sofa"
+    else:
+        return "llama"
+
+
+def word_transformer(word):
+    if word == "NOUN":
+        return random_noun()
+    elif word == "VERB":
+        return random_verb()
+    else:
+        return word[0]
+
+def process_madlib(mad_lib):
+    processed = ""
+    textlen = len(mad_lib)
+    # your code here
+    # you may find the built-in len function useful for this quiz
+    # documentation: https://docs.python.org/2/library/functions.html#len
+    i = 0
+    while i < textlen:
+        processed+=(mad_lib[i])
+        if processed[-4:] == "NOUN":
+            processed = processed.replace("NOUN", random_noun())
+        if processed[-4:] == "VERB":
+            processed = processed.replace("VERB", random_verb())
+        i += 1
+    return processed
+
+
+test_string_1 = "This is a good NOUN to use when you VERB your food"
+test_string_2 = "I'm going to VERB to the store and pick up a NOUN or two."
+print(process_madlib(test_string_1))
+print (process_madlib(test_string_2))
